49c11106c0235ea5dcb11de8077264962004c2c0
kvitek <vkugushev@gmail.com>
2019-04-12 19:12:10 +0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -7,9 +7,9 @@ Created on Tue Apr  9 14:41:38 2019
 
 #queries
 select_newest_reports = """
-select r.adsh, r.cik, r.fin_year, r.file_date, r.form from reports as r,
+select r.adsh, r.cik, r.fin_year as fy, r.file_date, r.form from reports as r,
 (
-	select cik, fin_year, max(file_date) as file_date, max(form) as form
+	select cik, fin_year, max(file_date) as file_date, max(adsh) as adsh
 	from reports
 	group by cik, fin_year
 ) m
@@ -17,5 +17,6 @@ where r.cik = m.cik
 	and r.fin_year = m.fin_year 
     and r.file_date = m.file_date
     and r.fin_year<=%(fy)s
-    and r.form = m.form;
+    and r.adsh = m.adsh;    
 """
+
